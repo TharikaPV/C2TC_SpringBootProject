0a49b3126bfb9107fccb7c9e456fa88195c5e9e7
pvtha <pvtha@tharipv>
2025-11-05 12:07:19 +0530

    commit

---
+++ DI_ioc_Autowired_project-4/src/main/java/com/tnsif/DI_ioc_Autowired_project_4/DiIocAutowiredProject4Application.java
@@ -0,0 +1,16 @@
+package com.tnsif.DI_ioc_Autowired_project_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DiIocAutowiredProject4Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext var = SpringApplication.run(DiIocAutowiredProject4Application.class, args);
+		Customer1 c1 = var.getBean(Customer1.class);
+		c1.show();
+	}
+
+}
